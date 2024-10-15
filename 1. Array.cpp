b899fe1e594a3128b05544e8a2055b2cb6d4a5d5
md santo <140914322+santosarkar@users.noreply.github.com>
2024-10-16 02:23:24 +0600

    Create 1. Array.cpp

---
+++ 1. Array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare and initialize an array of integers
+    int arr[5] = {1, 2, 3, 4, 5};
+    
+    // Loop to print each element of the array
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
